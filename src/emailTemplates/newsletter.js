18c0cb7d05eafe9da8a559160f105ec708b1900b
Brian <steelvoltage@gmail.com>
2023-02-25 08:56:20 -0500

    Update newsletter.js

--- src/emailTemplates/newsletter.js
+++ src/emailTemplates/newsletter.js
@@ -18,7 +18,7 @@ export async function sendMail({ mailto, subject, html, template }) {
 
 export function generateHtmlTemplate({ name, body, id, unsubPath }) {
 	const generic = import('./generic.html?raw');
-	console.log(name, body, id, unsubPath);
+	console.log(typeof name, typeof body, typeof id, typeof unsubPath);
 	return _.template(generic, {
 		name,
 		body,
