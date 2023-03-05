412729db727b9827961f085560d03417011560de
Brian <steelvoltage@gmail.com>
2023-03-05 16:17:04 -0500

    comments fix blog page

--- src/routes/blog/+page.server.js
+++ src/routes/blog/+page.server.js
@@ -1,9 +1,11 @@
 import { client, queries } from '../../db';
 import { curatePosts } from '../../helpers/curatePosts';
+import { getCommentNumForPosts } from '../../helpers/getComments';
 
 export async function load() {
 	const posts = await client.fetch(queries.posts);
-	const curatedPosts = curatePosts(posts);
+	const postsWithCommentNum = await getCommentNumForPosts(posts);
+	const curatedPosts = curatePosts(postsWithCommentNum);
 	return {
 		posts: curatedPosts
 	};
