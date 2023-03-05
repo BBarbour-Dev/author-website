9800cf778204c51d13f438e5f3d16de849b175bf
Brian <steelvoltage@gmail.com>
2023-03-05 15:32:15 -0500

    bugfix for comments rerendering, maybe a cdn issue?

--- src/db/index.js
+++ src/db/index.js
@@ -5,7 +5,7 @@ export const client = createClient({
 	projectId: 'r0ohisby',
 	dataset: 'author',
 	apiVersion: '2023-02-22',
-	useCdn: config.ENV === 'dev' ? false : true,
+	useCdn: false,
 	token: config.SANITY_AUTH_TOKEN
 });
 
